b1f6b8f38cc4a501fee3b498b522986b65b54ef6
syh8088 <syh8088@gmail.com>
2021-01-28 22:39:30 +0900

    start
    start

---
+++ src/main/java/kiwi/board/KiwiBoardApplication.java
@@ -0,0 +1,13 @@
+package kiwi.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwiBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kiwi/board/KiwiBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kiwi.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwiBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
